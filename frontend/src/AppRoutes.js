5c3c31c2f79728d556b68f77cff4ccefe866594d
Stevie <steviescripts52@gmail.com>
2023-10-19 11:03:30 -0700

    client side routes protected

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -5,6 +5,7 @@ import Signup from './components/Signup';
 import Product from './components/ProductCard/ProductDetail';
 import LandingPage from './components/LandingPage';
 import SearchResults from './components/SearchResults';
+import PrivateRoute from './PrivateRoute';
 
 function AppRoutes({handleLogin, handleSignup, handleLogout, products, setProducts}) {
 	return (
@@ -12,11 +13,21 @@ function AppRoutes({handleLogin, handleSignup, handleLogout, products, setProduc
 			<Route path='/' element={<Home />} />
 			<Route path='/login' element={<Login handleLogin={handleLogin} />} />
 			<Route path='/signup' element={<Signup handleSignup={handleSignup} />} />
-			<Route path='/user' handleLogout={handleLogout} element={<LandingPage />} />
-			<Route path='/search/product/:id' element={<Product />} />
-			<Route path='/search/products/:search' element={<SearchResults />} />
-			<Route path='/search/category/:id' element={<SearchResults />} />
-			<Route path='*' element={<h1>Not Found</h1>} />
+
+			<Route
+				path='/user'
+				element={<PrivateRoute element={<LandingPage handleLogout={handleLogout} />} />}
+			/>
+
+			<Route path='/search/product/:id' element={<PrivateRoute element={<Product />} />} />
+
+			<Route
+				path='/search/products/:search'
+				element={<PrivateRoute element={<SearchResults />} />}
+			/>
+			<Route path='/search/category/:id' element={<PrivateRoute element={<SearchResults />} />} />
+
+			<Route path='*' element={<h1>Page Not Found</h1>} />
 		</Routes>
 	);
 }
---
+++ frontend/src/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react';
+import {Navigate} from 'react-router-dom';
+import UserContext from './auth/UserContext';
+
+/** "Higher-Order Component" for private routes.
+ *
+ * In routing component, use these instead of <Route ...>. This component
+ * will check if there is a valid current user and only continues to the
+ * route if so. If no user is present, redirects to login form.
+ */
+
+function PrivateRoute({element}) {
+	const {currentUser} = useContext(UserContext);
+
+	console.debug('PrivateRoute', 'currentUser=', currentUser);
+
+	if (!currentUser) {
+		return <Navigate to='/login' />;
+	}
+
+	return element;
+}
+
+export default PrivateRoute;
+
